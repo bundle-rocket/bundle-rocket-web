563896ff3764a7a4b1698ea98cbc8665c050d7d0
lupengyu <lupengyu@baidu.com>
2017-05-30 14:16:25 +0800

    backup

--- src/app/ListView.js
+++ src/app/ListView.js
@@ -4,6 +4,11 @@
  */
 
 const React = require('react');
+const ToolBar = require('melon/ToolBar');
+const Button = require('melon/Button');
+const Icon = require('melon/Icon');
+const Title = require('melon/Title');
+const Card = require('melon/Card');
 
 class AppListView extends React.Component {
 
@@ -12,7 +17,22 @@ class AppListView extends React.Component {
     render() {
         return (
             <div className="app-view">
-                app list view
+                <ToolBar>
+                    <Button size="xl"><Icon icon="menu"/></Button>
+                    <Title level={4} className="flex">My Apps</Title>
+                    <Button size="xl"><Icon icon="power-settings-new" /></Button>
+                </ToolBar>
+                <Card>
+                    <div className="ui-card-title">
+                        应用列表
+                    </div>
+                    <div className="ui-card-content">
+                        list...
+                    </div>
+                </Card>
+                <Button size="xxl" variants={['floating', 'primary']}>
+                    <Icon icon="add" />
+                </Button>
             </div>
         );
     }
